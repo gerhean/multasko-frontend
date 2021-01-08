89f85425041b9b37ef4ffd38884c0053560384aa
Ger Hean <gerhean@live.com>
2021-01-08 20:47:12 +0800

    added get homedata

--- src/services/initData.js
+++ src/services/initData.js
@@ -1,4 +1,11 @@
-// import Axios from 'axios';
+import axios from 'axios';
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+const MEMO_URL = "https://multasko-backend.herokuapp.com/api/memo";
+// const CATEGORY_URL = "https://multasko-backend.herokuapp.com/api/category";
+// let lastMemoTime = 1610087437;
 
 let multaskoHomeData = {
 	data: [
@@ -162,8 +169,63 @@ let multaskoCategoriesData = {
 	],
 };
 
-export function init() {
+function hmsToSeconds(hms) {
+	const a = hms.split(':'); // split it at the colons
+	// minutes are worth 60 seconds. Hours are worth 60 minutes.
+	return (+a[0]) * 3600 + (+a[1]) * 60 + (+a[2]); 
+}
+
+function dateToString(dateStr) {
+	const a = dateStr.split('-'); // split it at the colons
+	if(a[2][0] == "0") {
+		a[2] = a[2][1];
+	}
+	// minutes are worth 60 seconds. Hours are worth 60 minutes.
+	return monthNames[+a[1] - 1] + " " + a[2]; 
+}
+
+export async function init() {
 	// call backend
+	const response = await axios ({
+		url: MEMO_URL,
+		method: "GET"
+	});
+
+	const memos = response.data.memos;
+	let curDate = memos[0].date_posted[0];
+	let curTime = hmsToSeconds(memos[0].date_posted[1]);
+	const homeData = [];
+	let dayGroup = [];
+	let timeBoxGroup = [];
+	for(const memo of memos) {
+		const seconds = hmsToSeconds(memo.date_posted[1]);
+		if (memo.date_posted[0] != curDate) {
+			dayGroup.push(timeBoxGroup);
+			homeData.push({
+				date: dateToString(curDate),
+				memos: dayGroup,
+			})
+			dayGroup = [];
+			timeBoxGroup = [];
+		} else if (curTime - seconds > 300) {
+			dayGroup.push(timeBoxGroup);
+			timeBoxGroup = [];
+		}
+		curTime = seconds;
+		timeBoxGroup.push({
+			text: memo.text,
+			priority_level: memo.priority_level,
+			date_posted: memo.date_posted
+		});
+	}
+
+	dayGroup.push(timeBoxGroup);
+	homeData.push({
+		date: dateToString(curDate),
+		memos: dayGroup,
+	});
+
+	multaskoHomeData.data = homeData;
 }
 
 const service = {
@@ -171,4 +233,5 @@ const service = {
 	multaskoCategoriesData: multaskoCategoriesData,
 }
 
+init();
 export default service;
