e0350cb00baa3f7ada5ed405778da2c512606dfa
Ger Hean <gerhean@live.com>
2021-01-08 22:06:13 +0800

    add postMemo

--- src/services/initData.js
+++ src/services/initData.js
@@ -210,6 +210,7 @@ export async function initHomeData() {
 	for(const memo of memos) {
 		const seconds = hmsToSeconds(memo.date_posted[1]);
 		if (memo.date_posted[0] != curDate) {
+			timeBoxGroup.reverse();
 			dayGroup.push(timeBoxGroup);
 			homeData.push({
 				date: dateToString(curDate),
@@ -218,15 +219,12 @@ export async function initHomeData() {
 			dayGroup = [];
 			timeBoxGroup = [];
 		} else if (curTime - seconds > 300) {
+			timeBoxGroup.reverse();
 			dayGroup.push(timeBoxGroup);
 			timeBoxGroup = [];
 		}
 		curTime = seconds;
-		timeBoxGroup.push({
-			text: memo.text,
-			priority_level: memo.priority_level,
-			date_posted: memo.date_posted
-		});
+		timeBoxGroup.push(memo);
 	}
 
 	dayGroup.push(timeBoxGroup);
@@ -248,6 +246,23 @@ export async function initCategoriesData() {
 	populateListOfCategories();
 }
 
+export async function postMemo(text, priority_level) {
+	const response = await axios.post(MEMO_URL, 
+		{"text": text, "priority_level": priority_level});
+	const newMemo = response.data;
+	const lastPostedDate = multaskoHomeData.data[0].memos[0][0].date_posted;
+	if (newMemo.date_posted[0] != lastPostedDate[0]) {
+		multaskoHomeData.data.unshift({
+			date: dateToString(newMemo.date_posted[0]),
+			memos: [[newMemo]],
+		})
+	} else if (hmsToSeconds(newMemo.date_posted[1]) - hmsToSeconds(lastPostedDate[1]) > 300) {
+		multaskoHomeData.data[0].memos.unshift([newMemo]);
+	} else {
+		multaskoHomeData.data[0].memos[0].push(newMemo);
+	}
+}
+
 populateListOfCategories(); // remove after API function is completed
 
 const service = {
@@ -256,4 +271,5 @@ const service = {
 	multaskoListOfCategories: multaskoListOfCategories, 
 }
 
+// putMemo("a test message", 1);
 export default service;
